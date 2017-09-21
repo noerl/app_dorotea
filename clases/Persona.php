c365ba8b0a67c66c5c6af72ada740c422fbbeb5a
Luis Fernando Díaz Vargas <fdsolinf@gmail.com>
2017-09-20 21:34:10 -0500

    Commit
    Esta correcta.

--- clases/Persona.php
+++ clases/Persona.php
@@ -1,16 +1,91 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+require_once '../configuracion/Conexion.php';
 /**
  * Description of Persona
  *
- * @author fdweb
+ * @author Fernando Díaz
  */
 class Persona {
-    //put your code here
+    // Tabla
+    const TABLA = "01_usuario";
+    
+    // Atributos
+    private $id;
+    private $nombre;
+    private $apeliidos;
+    private $documento;
+    private $correo;
+    private $telefono;
+    private $estado;
+    
+    // Get y Set
+    function getId() {
+        return $this->id;
+    }
+
+    function getNombre() {
+        return $this->nombre;
+    }
+
+    function getApeliidos() {
+        return $this->apeliidos;
+    }
+
+    function getDocumento() {
+        return $this->documento;
+    }
+
+    function getCorreo() {
+        return $this->correo;
+    }
+
+    function getTelefono() {
+        return $this->telefono;
+    }
+
+    function getEstado() {
+        return $this->estado;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    function setApeliidos($apeliidos) {
+        $this->apeliidos = $apeliidos;
+    }
+
+    function setDocumento($documento) {
+        $this->documento = $documento;
+    }
+
+    function setCorreo($correo) {
+        $this->correo = $correo;
+    }
+
+    function setTelefono($telefono) {
+        $this->telefono = $telefono;
+    }
+
+    function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+        
+    // Constructor
+    function __construct($nombre, $apeliidos, $documento, $correo, $telefono, $estado, $id = null) {
+        $this->nombre = $nombre;
+        $this->apeliidos = $apeliidos;
+        $this->documento = $documento;
+        $this->correo = $correo;
+        $this->telefono = $telefono;
+        $this->estado = $estado;
+        $this->id = $id;
+    }
+
+    // Metodos de la clase
 }
